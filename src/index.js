4702534c6a12f3d6f55ad3a1ae64010ab5f458c5
Niilo Keinänen <niilo.keinanen@lightningchart.com>
2025-11-12 09:27:36 +0200

    LCJS v8.1.0

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ const {
 } = lcjs
 
 const dashboardRows = 4
-// NOTE: Using `Dashboard` is no longer recommended for new applications. Find latest recommendations here: https://lightningchart.com/js-charts/docs/basic-topics/grouping-charts/
+// NOTE: Using `Dashboard` is no longer recommended for new applications. Find latest recommendations here: https://lightningchart.com/js-charts/docs/more-guides/grouping-charts/
 const dashboard = lightningChart({
             resourcesBaseUrl: new URL(document.head.baseURI).origin + new URL(document.head.baseURI).pathname + 'resources/',
         }).Dashboard({
@@ -381,9 +381,12 @@ dashboard.addEventListener('resize', (event) => {
                 schema: {
                     x: { pattern: null },
                     y: { pattern: null },
+                    lookupValue: { pattern: null },
+                    size: { pattern: null },
                 },
             })
             .setPointFillStyle(new PalettedFill({ lut: lutNewCasesPerMillion }))
+            .setPointStrokeStyle(emptyLine)
             .setPointerEvents(false)
 
         let regions = []
